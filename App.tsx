2b67fb39f7bcae8b3dd20a92309e28e667117a92
Javier Midence <javier.midence22@gmail.com>
2023-10-19 15:34:13 -0600

    Changes for Integration

--- App.tsx
+++ App.tsx
@@ -1,84 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
 import {
-  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Modal,
+  TextInput,
 } from 'react-native';
+import Button from './src/Components/Button';
+// import Clipboard from '@react-native-clipboard/clipboard';
 
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import paypalApi from './src/apis/paypalApi';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import WebView, {WebViewNavigation} from 'react-native-webview';
+import queryString from 'query-string';
+import Toast from 'react-native-simple-toast';
 
 function App(): JSX.Element {
+  const [loading, setLoading] = useState(false);
+  const [paypalUrl, setPaypalUrl] = useState<string | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [partnerId, setPartnerId] = useState<string | null>(null);
+  const [partnerUrl, setPartnerUrl] = useState<string | null>(null);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
-  const test = async () => {
+  const handleClick = async () => {
     console.log('click');
     try {
+      setLoading(true);
       const token = await paypalApi.generateToken();
-      const res = await paypalApi.createOrder({token});
+      const res = await paypalApi.createOrder({
+        token,
+        sellerPayerId: partnerId || '',
+      });
+      setAccessToken(token);
       console.log('response', res);
+
+      if (res?.links?.length) {
+        const url = res.links.find(item => item.rel === 'approve');
+        if (url?.href) {
+          setPaypalUrl(url.href);
+        } else {
+          Toast.show('Error on payment, no url found', Toast.SHORT, {
+            backgroundColor: '#d16564',
+            textColor: 'white',
+          });
+        }
+      }
     } catch (e) {
       console.error(e);
     }
-    // await fetch('http://10.0.2.2:3000/getClientToken', {
-    //   method: 'GET',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'content-type': 'application/json',
-    //   },
-    // })
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log('response');
-    //     return data;
-    //   });
+    setLoading(false);
+  };
+
+  const handleClickPartner = async () => {
+    console.log('click');
+    try {
+      setLoading(true);
+      const token = await paypalApi.generateToken();
+      const res = await paypalApi.addPartner({token});
+      setAccessToken(token);
+      console.log('add partner', res);
+
+      if (res?.links?.length) {
+        const url = res.links.find(item => item.rel === 'action_url');
+        const _self = res.links.find(item => item.rel === 'self');
+        if (url?.href) {
+          setPartnerUrl(url.href);
+          console.log('partner url', url.href);
+        }
+        if (_self?.href) {
+          const splitedUrl = _self.href.split('/');
+          const partId = splitedUrl[splitedUrl.length - 1];
+          console.log('self', _self.href);
+          console.log('partner id', partId);
+          setPartnerId(partId);
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
+
+  const onUrlChange = (webViewState: WebViewNavigation) => {
+    console.log('web view state', webViewState);
+    if (webViewState.url.includes('https://example.com/cancel')) {
+      clearPaypalState();
+      Toast.show('Cancelled payment', Toast.SHORT, {
+        backgroundColor: '#d16564',
+        textColor: 'white',
+      });
+      return;
+    }
+    if (webViewState.url.includes('https://example.com/return')) {
+      const urlValues = queryString.parseUrl(webViewState.url);
+      console.log('url values', urlValues);
+      const {token} = urlValues.query;
+      if (token) {
+        paymentSucess(token as string);
+      }
+    }
+  };
+
+  const paymentSucess = async (id: string) => {
+    try {
+      if (accessToken) {
+        const res = paypalApi.capturePayment({
+          id,
+          token: accessToken,
+          sellerPayerId: partnerId || '',
+        });
+        console.log('payment resp', res);
+        clearPaypalState();
+        Toast.show('Payment Successfull!', Toast.LONG);
+      }
+    } catch (e) {
+      Toast.show('Error on payment', Toast.SHORT, {
+        backgroundColor: '#d16564',
+        textColor: 'white',
+      });
+    }
+  };
+
+  const clearPaypalState = () => {
+    setPaypalUrl(null);
+    setAccessToken(null);
+    setLoading(false);
   };
 
   return (
@@ -95,38 +151,55 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            <Button
-              title="Paypay"
-              color="#0f4fa3"
-              onPress={() => {
-                test();
-              }}
-            />
-          </Section>
+          <Button
+            text="Add Partner"
+            btnStyle={{backgroundColor: '#009435', margin: 16}}
+            isLoading={loading}
+            onPress={() => {
+              handleClickPartner();
+            }}
+          />
+          <Button
+            text="Paypal"
+            disabled={!partnerId}
+            btnStyle={{backgroundColor: '#0f4fa3', margin: 16}}
+            isLoading={loading}
+            onPress={() => {
+              handleClick();
+            }}
+          />
+
+          <Modal visible={!!paypalUrl}>
+            <View style={{flex: 1}}>
+              {paypalUrl && (
+                <WebView
+                  source={{uri: paypalUrl}}
+                  onNavigationStateChange={onUrlChange}
+                />
+              )}
+            </View>
+          </Modal>
+          <Modal visible={!!partnerUrl}>
+            <View>
+              <TextInput>Url To send: {partnerUrl}</TextInput>
+              {/* <Button
+                text="Copy url"
+                onPress={() => {
+                  Clipboard.setString(partnerUrl || '');
+                }}
+              /> */}
+              <Button
+                text="Close"
+                onPress={() => {
+                  setPartnerUrl(null);
+                }}
+              />
+            </View>
+          </Modal>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/apis/paypalApi.ts
+++ src/apis/paypalApi.ts
@@ -3,6 +3,25 @@ const base64 = require('base-64');
 
 const clientId = Config.PAYPAL_CLIENT_ID;
 const secretKey = Config.PAYPAL_SECRET_KEY;
+const bnCode = Config.PAYPAL_BN_CODE;
+
+export interface CreateOrderResponse {
+  id: string;
+  links: Link[];
+  status: string;
+}
+
+export interface Link {
+  href: string;
+  method: string;
+  rel: string;
+}
+
+export type CapturePaymentParams = {
+  id: string;
+  token?: string;
+  sellerPayerId: string;
+};
 
 const orderDetail = {
   intent: 'CAPTURE',
@@ -11,11 +30,20 @@ const orderDetail = {
       items: [
         {
           name: 'T-Shirt',
-          description: 'Green XL',
+          description: 'Red XL',
           quantity: '1',
           unit_amount: {
             currency_code: 'USD',
-            value: '100.00',
+            value: '80.00',
+          },
+        },
+        {
+          name: 'Socks',
+          description: 'Coca-Cola socks',
+          quantity: '2',
+          unit_amount: {
+            currency_code: 'USD',
+            value: '10.00',
           },
         },
       ],
@@ -37,7 +65,32 @@ const orderDetail = {
   },
 };
 
-const generateToken = (): Promise<string> => {
+const bodyPartner = {
+  // tracking_id: 'TRACKING-ID',
+  operations: [
+    {
+      operation: 'API_INTEGRATION',
+      api_integration_preference: {
+        rest_api_integration: {
+          integration_method: 'PAYPAL',
+          integration_type: 'THIRD_PARTY',
+          third_party_details: {
+            features: ['PAYMENT', 'REFUND'],
+          },
+        },
+      },
+    },
+  ],
+  products: ['EXPRESS_CHECKOUT'],
+  legal_consents: [
+    {
+      type: 'SHARE_DATA_CONSENT',
+      granted: true,
+    },
+  ],
+};
+
+const generateToken = async (): Promise<string> => {
   const headers = new Headers();
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
   headers.append(
@@ -63,10 +116,16 @@ const generateToken = (): Promise<string> => {
   });
 };
 
-const createOrder = ({token = ''}) => {
+const createOrder = async ({
+  token = '',
+  sellerPayerId = '',
+}): Promise<CreateOrderResponse> => {
   const headers = new Headers();
+  const assertionToken = await authAssertion(sellerPayerId);
   headers.append('Content-Type', 'application/json');
   headers.append('Authorization', `Bearer ${token}`);
+  headers.append('PayPal-Auth-Assertion', assertionToken);
+  headers.append('PayPal-Partner-Attribution', bnCode || '');
 
   var requestOptions = {
     method: 'POST',
@@ -78,7 +137,90 @@ const createOrder = ({token = ''}) => {
     fetch(`${Config.PAYPAL_BASE_URL}/v2/checkout/orders`, requestOptions)
       .then(response => response.json())
       .then(result => {
-        resolve(result.access_token);
+        resolve(result);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+const capturePayment = async ({
+  id,
+  token = '',
+  sellerPayerId = '',
+}: CapturePaymentParams): Promise<CreateOrderResponse> => {
+  const headers = new Headers();
+  const assertionToken = await authAssertion(sellerPayerId);
+  headers.append('Content-Type', 'application/json');
+  headers.append('Authorization', `Bearer ${token}`);
+  headers.append('PayPal-Auth-Assertion', assertionToken);
+  headers.append('PayPal-Partner-Attribution', bnCode || '');
+
+  var requestOptions = {
+    method: 'POST',
+    headers: headers,
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${Config.PAYPAL_BASE_URL}/v2/checkout/orders/${id}/capture`,
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result => {
+        resolve(result);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+// generate Auth Assertion
+async function authAssertion(sellerPayerId: string): Promise<string> {
+  const jwt = getAuthAssertionValue(sellerPayerId);
+  return jwt;
+}
+function getAuthAssertionValue(sellerPayerId: string) {
+  const header = {
+    alg: 'none',
+  };
+  const encodedHeader = base64url(header);
+  const payload = {
+    iss: clientId,
+    payer_id: sellerPayerId,
+  };
+  const encodedPayload = base64url(payload);
+  return `${encodedHeader}.${encodedPayload}.`;
+}
+
+function base64url(json: {alg?: string; iss?: string; payer_id?: string}) {
+  return btoa(JSON.stringify(json))
+    .replace(/[=]+$/, '')
+    .replace(/+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const addPartner = async ({token = ''}): Promise<CreateOrderResponse> => {
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('Authorization', `Bearer ${token}`);
+
+  var requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(bodyPartner),
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${Config.PAYPAL_BASE_URL}/v2/customer/partner-referrals`,
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result => {
+        resolve(result);
       })
       .catch(error => {
         reject(error);
@@ -89,4 +231,6 @@ const createOrder = ({token = ''}) => {
 export default {
   generateToken,
   createOrder,
+  capturePayment,
+  addPartner,
 };
